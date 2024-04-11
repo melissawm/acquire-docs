e24920c19425cbc02bff2a59402b2d5b31ec4704
Melissa Weber Mendonça <melissawm@gmail.com>
2024-04-11 18:11:29 -0300

    Make convert.sh script more robust
    Co-authored-by: Alan Liddell <alan.c.liddell@gmail.com>

--- .github/workflows/convert.sh
+++ .github/workflows/convert.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 echo "Converting all .md tutorials to .py files..."
-cd ./docs/tutorials;
+TUTS_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/../../docs/tutorials"
+pushd ${TUTS_DIR}
 for file in *.md; do
     if [ "$file" != "index.md" ]; then
         jupytext --to py $file
     fi
 done
-cd -
+popd
 echo "Done."
