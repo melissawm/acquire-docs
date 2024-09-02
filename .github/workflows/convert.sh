fe0bf50b6327009fd7c4466d60cd7b497d46798a
Melissa Weber Mendonça <melissawm@gmail.com>
2024-09-02 10:27:55 -0300

    Fix conversion script

--- .github/workflows/convert.sh
+++ .github/workflows/convert.sh
@@ -2,10 +2,14 @@
 echo "Converting all .md tutorials to .py files..."
 TUTS_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/../../docs/tutorials"
 pushd ${TUTS_DIR}
-for file in *.md; do
-    if [ "$file" != "index.md" ] && [ "$file" != "livestream.md" ]; then
-        jupytext --to py $file
-    fi
+for subdir in */; do
+    pushd $subdir
+    for file in *.md; do
+        if [ "$file" != "index.md" ] && [ "$file" != "livestream.md" ]; then
+            jupytext --to py $file
+        fi
+    done
+    popd
 done
 popd
 echo "Done."
