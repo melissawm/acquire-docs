546c0f9b7b2f89a3b1d09389f7738d32ba6f3b5f
Melissa Weber Mendonça <melissawm@gmail.com>
2025-02-11 15:04:21 -0300

    Update pages

--- dev/api_reference/acquire_zarr_c_api/html/search/variables_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['type_0',['type',['../struct_zarr_dimension_properties.html#aabb68f78e50454e92ed877cecfe10333',1,'ZarrDimensionProperties']]]
-];
--- dev/api_reference/acquire_zarr_c_api/html/search/variables_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['version_0',['version',['../struct_zarr_stream_settings.html#ada644bd2cf3dcd6e19d6cadfceac94e0',1,'ZarrStreamSettings']]]
-];
